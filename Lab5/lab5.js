724c879820543b868d40f00c253f22e08ac82cd4
Jessica Matsuda <jessi7@hawaii.edu>
2023-09-21 11:29:01 -1000

    UPDATE

--- Lab5/lab5.js
+++ Lab5/lab5.js
@@ -6,8 +6,10 @@ let month_of_birth=6;
 
 //define calculation
 let calc1 = age + fav_num / day_of_birth * month_of_birth;
+
 let calc2 = (age + fav_num) / day_of_birth * month_of_birth;
 
 //output calc 1 & 2 to the DOM
 document.getElementById("result1").innerHTML = calc1;
+
 document.getElementById("result2").innerHTML = calc2;

5eaccb8aeecc43186822c66ea78fb77a9c05c8e5
Jessica Matsuda <jessi7@hawaii.edu>
2023-10-01 22:03:41 -1000

    update

--- Lab6/SmartPhoneProducts1_4/products_display6.js
+++ Lab6/SmartPhoneProducts1_4/products_display6.js
@@ -1,8 +1,6 @@
 //define store name and push it to the DOM in the top title
 const store_name="Jessica Matsuda";
-let first_name = "Jessica";
-let last_name = "Matsuda";
-top_title.innerHTML=(store_name+"'s Used Smart Phone Store");
+
 
 
 //send store name info to the footer title
@@ -22,12 +20,11 @@ spins_span.innerHTML=spins;
 //this should be two functions or more but... it changes the item name to rotate for the images and updates
 function changeClassName(element){
     if(element.className=='item'){
-        spins=spins+1;
         element.className = 'item rotate';
+        spins=spins+1;
 
     }
 
-    
     //spins=spins+1;
     if(spins<2*hits&&hits<spins){
         //wins=true;
@@ -41,11 +38,33 @@ function changeClassName(element){
     hit_spin_span.innerHTML=(hits/spins).toFixed(2);
 }
 
+
+// -- Winning progress depends on hits/spins
+hits_spins_ratio = hits/spins;
+let hits_spins_ratio=hits/spins;
+let progress;
+if ( hits_spins_ratio > 0 ) {
+    progress = 'On your way!';
+    if ( hits_spins_ratio >= 0.25 ) {
+        progress = 'Almost there!';
+        if ( hits_spins_ratio >= 0.5 ) {
+            if( hits < spins) { 
+                progress = 'You win!';
+            }
+        }
+    }
+}
+else {
+    progress = 'Get going!' ;
+}
+win_span.innerHTML=progress;
+
+
 //this should be two functions or more but... it changes the item name by removing the rotate for the images and
 function resetClassName(element){
     if(element.className=='item rotate'){
-        hits=hits+=2;
         element.className='item';
+        hits=hits+=2;
     } else{
         changeClassName(element);
     }
@@ -60,6 +79,37 @@ function resetClassName(element){
     //win_span.innerHTML=wins;
     win_span.innerHTML=over_half;
     spins_span.innerHTML=spins;
-    hit_spin_span.innerHTML=(hits/spins).toFixed(2);
+    hit_spin_span.innerHTML=(hits/spins).toFixed(2)
+    
+     // -- Winning progress depends on hits/spins
+     let hits_spins_ratio = hits/spins;
+     let progress
+     /*if ( hits_spins_ratio > 0 ) {
+         progress = 'On your way!';
+         if ( hits_spins_ratio >= 0.25 ) {
+             progress = 'Almost there!';
+             if ( hits_spins_ratio >= 0.5 ) {
+                 if( hits < spins) { 
+                     progress = 'You win!';
+                 }
+             }
+         }
+     }
+     else {
+         progress = 'Get going!' ;
+     }*/
+ 
+     if(hits_spins_ratio >= 0.5 && hits<spins) {
+             progress='You win!';
+     } else if(hits_spins_ratio >= 0.25) {
+         progress='Almost there!';
+     } else if(hits_spins_ratio>0) {
+         progress='On your way!';
+     }
+     else {
+         progress='Get going!';
+     }
+     win_span.innerHTML=progress;
+ 
 
 }

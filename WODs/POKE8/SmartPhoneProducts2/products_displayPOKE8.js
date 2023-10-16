df0902bf5638507fccf5bb0b9abd6b4fcdcaeb7a
Jessica Matsuda <jessi7@hawaii.edu>
2023-10-15 23:53:11 -1000

    update

--- WODs/POKE8/SmartPhoneProducts2/products_displayPOKE8.js
+++ WODs/POKE8/SmartPhoneProducts2/products_displayPOKE8.js
@@ -12,7 +12,7 @@ let hits=0;
 let spins=0;
 //let wins;
 let over_half=false;
-hits_san.innerHTML=hits;
+hits_span.innerHTML=hits;
 spins_span.innerHTML=spins;
 
 let name1 = "HTC";
@@ -35,73 +35,49 @@ let name5 = "Blackberry";
 let price5 = 10.00;
 let image5 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
 
-for(let i=1; eval("typeof name"+i) != 'undefined'; i++){
+for(let i=1; eval("typeof name" +i) !='undefined'; i++) {
     document.querySelector('.main').innerHTML += `
-    <section class="item" onmouseover="changeClassName(this);"
-    onclick="resetClassName(this);">
-        <h2>${eval("name"+i)}</h2>
-        <p>$${eval("price"+i)}</p>
-        <img src="${eval("image"+i)}" />
-    </section>   
-    `;
+    <section class="item" onmouseover="changeClassName(this);" onclick="resetClassName(this);">
+    <h2> ${eval("name" +i)}</h2>
+    <p>$${eval("price" +i)}</p>
+    <img src="${eval("image" +i)}"/> 
+    </section>  
+`
 }
 
-
-
-//this should be two functions or more but... it changes the item name to rotate for the images and updates
-function changeClassName(element){
-    if(element.className=='item'){
-        element.className = 'item rotate';
-        spins=spins+1;
-
-    }
-
-    //spins=spins+1;
-    if(spins<2*hits&&hits<spins){
-        //wins=true;
-        over_half=true;
-    } else {
-        //wins=false;
-    }
-    //win_span.innerHTML=wins;
-    win_span.innerHTML=over_half;
-    spins_span.innerHTML=spins;
-    hit_spin_span.innerHTML=(hits/spins).toFixed(2);
+//changes item name to rotate for images and updates hits & spins
+function changeClassName(element) {
+if(element.className=='item'){
+    spins=spins+1
+    element.className = 'item rotate';
+} 
+//spins=spins+1;
+if(spins<2*hits&&hits<spins) {
+    //wins=true;
+    over_half=true;
+} else {
+    //wins=false;
 }
-
+//win_span.innerHTML=wins;
+win_span.innerHTML=over_half;    
+spins_span.innerHTML = spins; 
+hit_spin_span.innerHTML=Number(hits/spins).toFixed(2)
 
 // -- Winning progress depends on hits/spins
 hits_spins_ratio = hits/spins;
-let hits_spins_ratio=hits/spins;
-let progress;
-/*if ( hits_spins_ratio > 0 ) {
-    progress = 'On your way!';
-    if ( hits_spins_ratio >= 0.25 ) {
+if (hits_spins_ratio > 0 && hits_spins_ratio < 0.25) {
+    progress = 'On your way!'; 
+    } else if (hits_spins_ratio >= 0.25 && hits_spins_ratio < 0.5) {
         progress = 'Almost there!';
-        if ( hits_spins_ratio >= 0.5 ) {
-            if( hits < spins) { 
-                progress = 'You win!';
-            }
-        }
-    }
-}
-else {
-    progress = 'Get going!' ;
-} */
-if(hits_spins_ratio >= 0.5 && hits<spins) {
-    progress='You win!';
-} else if(hits_spins_ratio >= 0.25) {
-progress='Almost there!';
-} else if(hits_spins_ratio>0) {
-progress='On your way!';
-}
-else {
-progress='Get going!';
+    } else if (hits_spins_ratio >= 0.5 && hits<spins) {
+        progress = 'You win!';
+} else {
+    progress = 'Get going!';
 }
 win_span.innerHTML=progress;
+}
 
 
-//this should be two functions or more but... it changes the item name by removing the rotate for the images and
 function resetClassName(element){
     if(element.className=='item rotate'){
         element.className='item';
